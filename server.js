b23defa3e6c6fd6b951842b84bb00b5da614c11d
Svenpire <svenpire@gmail.com>
2022-04-23 20:06:21 -0600

    feat(backend):JWT

--- server.js
+++ server.js
@@ -5,14 +5,20 @@ const path = require("path");
 const cors = require("cors");
 const { logger } = require("./middleware/logEvents");
 const errorHandler = require("./middleware/errorHandler");
+const verifyJWT = require("./middleware/verifyJWT");
+const cookieParser = require("cookie-parser");
 const corsOptions = require("./config/corsOptions");
+const credentials = require("./middleware/credentials");
 
 const PORT = process.env.PORT || 3500;
 
 // MIDDLEWARE
 // custom middleware logger
 app.use(logger);
-
+// Handle options credentials check - before CORS!
+// and fetch cookies credentials requiement
+app.use(credentials);
+// Cross Origin Resource Sharing
 app.use(cors(corsOptions));
 
 // .use() applies middleware to all incoming routes
@@ -20,6 +26,8 @@ app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: false }));
 // handles NOT form json data
 app.use(express.json());
+// middleware for cookies
+app.use(cookieParser());
 
 // serve static files (such as css)
 app.use(express.static(path.join(__dirname, "./public")));
@@ -30,7 +38,11 @@ app.use("/subdir", express.static(path.join(__dirname, "./public")));
 app.use("/", require("./routes/root"));
 app.use("/register", require("./routes/register"));
 app.use("/auth", require("./routes/auth"));
+app.use("/refresh", require("./routes/refresh"));
+app.use("/logout", require("./routes/logout"));
 app.use("/subdir", require("./routes/subdir"));
+// All routes below will use verifyJWT
+app.use(verifyJWT);
 app.use("/employees", require("./routes/api/employees"));
 
 // app.use('/')
